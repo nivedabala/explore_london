d502a2b60c45ef0d7c63d2f254540b8c5c3aaa91
Curt <52674197+curtiskennedy@users.noreply.github.com>
2020-11-21 12:24:45 -0700

    art now reads in both names and addresses

--- pathplanner.py
+++ pathplanner.py
@@ -8,7 +8,7 @@ user_wants_trees = True
 return_to_start = True
 total_distanceKM = 5
 destinations = []
-art = []
+art = {}
 parks = []
 #testing
 dest = '378 Horton St E, London, ON N6B 1L7'
@@ -31,7 +31,7 @@ def readParks(parks):
             if line_count == 0:
                 line_count += 1
             else:
-                # add address to art[]
+                # add address to parks[]
                 parks.append(row[2])
                 line_count += 1
         print("file had %d lines" % line_count)
@@ -46,10 +46,10 @@ def readArt(art):
             if line_count == 0:
                 line_count += 1
             else:
-                # add address to art[]
-                art.append(row[5])
+                # add name and address to art{}
+                art[row[3]] = row[5]
                 line_count += 1
-        print("file had % lines" % line_count)
+        print("file had %d lines" % line_count)
         print(art)
 
 # def sortParks():
@@ -62,5 +62,6 @@ def readArt(art):
 #     # most visited destinations
 
 # TESTING
+#calcDist()
 #readParks(parks)
 #readArt(art)
