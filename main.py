b906a0192c24d84eff5441fd131f93cb41b1713f
Julia Groza <jgroza24@gmail.com>
2020-11-21 11:06:11 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/question")
+def question():
+    return render_template("question.html")
+
+@app.route("/display")
+def display():
+    return render_template("display.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+    
