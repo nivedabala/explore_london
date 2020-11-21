2dd0c824ae3d5a7600dd26add45cf0fb756662fc
Julia Groza <jgroza24@gmail.com>
2020-11-21 17:36:00 -0500

    save

--- api.py
+++ api.py
@@ -1,26 +1,19 @@
 from flask import Flask, render_template
-import csv
+#import csv
 
 app = Flask(__name__)
 
-#@app.route('/', methods=['GET'])
-#def api():
-    #return {
-        #'userId':1,
-        #'title': 'Flask React Application',
-        #'completed': False
-    #}
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route("/question")
+@app.route('/questions.html')
 def question():
     return render_template("questions.html")
 
-@app.route("/display")
+@app.route("/display.html")
 def display():
     return render_template("display.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
