caa4cd81bfba6bc9e843da44f02c361051978685
swan484 <swan484@uwo.ca>
2020-11-21 15:55:22 -0500

    dont use

--- json_io.py
+++ json_io.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/output")
 def output():
-	return "Hello World!"
+	return render_template("home.html")
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='localhost', debug=True)
