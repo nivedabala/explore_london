accc78e322f0bce00c9000b2e30de79c20490e3f
Julia Groza <jgroza24@gmail.com>
2020-11-21 19:10:09 -0500

    test

--- api.py
+++ api.py
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
-from flask import Flask, render_template
-#import csv
-
-app = Flask(__name__)
-
-=======
 from flask import Flask, request, render_template, redirect, url_for
 import csv
 
 app = Flask(__name__)
 
+from pathplanner import calcDist
+from pathplanner import readParks
+from pathplanner import readArt
+from pathplanner import findFirstPark
+from pathplanner import plan
 
 # @app.route('/', methods=['GET'])
 # def api():
@@ -18,18 +16,10 @@ app = Flask(__name__)
 # 'title': 'Flask React Application',
 # 'completed': False
 # }
->>>>>>> 74c333c638a201f7e96fb14bc7e71585de165598
 @app.route('/')
 def home():
     return render_template("home.html")
 
-<<<<<<< HEAD
-@app.route('/questions.html')
-def question():
-    return render_template("questions.html")
-
-@app.route("/display.html")
-=======
 
 @app.route("/questions", methods=['GET', 'POST'])
 def questions():
@@ -40,7 +30,6 @@ def questions():
 
 
 @app.route("/display", methods=['GET', 'POST'])
->>>>>>> 74c333c638a201f7e96fb14bc7e71585de165598
 def display():
     if request.method == 'POST':
         return redirect(url_for('home'))
@@ -49,8 +38,4 @@ def display():
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    app.run(debug=True)
-=======
     app.run(debug=False, host='localhost',port=5001)
->>>>>>> 74c333c638a201f7e96fb14bc7e71585de165598
